4b07f8291c2d2e6ed4766f0cf55b7f5667d73ae5
David Thomas & Rachel Munoz <pair+kawikathomas.rachelumunoz@devbootcamp.com>
2016-09-26 14:35:36 -0700

    start building user model and migrations

---
+++ db/migrate/20160926212545__add_personal_info.rb
@@ -0,0 +1,6 @@
+class AddPersonalInfo < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string, null:false
+    add_column :users, :admin, :boolean, :default => false
+  end
+end
